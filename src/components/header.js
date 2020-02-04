f7500d52a96412ea89778e7dcce45486bda6de33
Simon Lawrenson <simon@optimising.com.au>
2020-02-05 09:28:55 +1100

    Add responsive navigation support

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,22 @@
 import React from "react"
-// import { Link } from "gatsby"
-// import TransitionLink from "gatsby-plugin-transition-link"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 import Navigation from "./navigation"
 
 export default() => 
   <header className="header">
     <div className="container container-logo">
-      <div className="col">
+      <div className="col col-logo">
         <p className="logo">
-        <AniLink cover
-      to="/"
-      direction="right"
-      duration={1}
-      bg="
-        #ff414e     /* color */
-    ">Simon Lawrenson</AniLink>
+          <AniLink cover
+            to="/"
+            direction="right"
+            duration={1}
+            bg="
+              #ff414e     /* color */
+          ">Simon Lawrenson</AniLink>
         </p>
         <p className="tagline">Melbourne based developer.</p>
       </div>
-      <div className="col">
-        <Navigation />
-      </div>
+      <Navigation />
     </div>
   </header>
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,28 +1,40 @@
-import React from "react"
-// import { Link } from "gatsby"
+import React, { useState } from "react"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
 
-export default()  =>
-  <nav className="navigation">
-    <AniLink cover
-      to="/tech"
-      direction="right"
-      duration={1}
-      bg="
-        #ff414e     /* color */
-    ">Tech stack</AniLink>
-    <AniLink cover
-      to="/projects"
-      direction="right"
-      duration={1}
-      bg="
-        #ff414e     /* color */
-    ">Projects</AniLink>
-    <AniLink cover
-      to="/contact"
-      direction="right"
-      duration={1}
-      bg="
-        #ff414e     /* color */
-    ">Get in touch</AniLink>
-  </nav>
+
+function Navigation () {
+  
+  const [isExpanded, toggleExpansion] = useState(false)
+  
+  return (
+    <div className={`${ isExpanded ? `nav-open` : `nav-closed` } col col-navigation`}>
+      <button className="navigation-toggler" type="button" data-toggle="offcanvas" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation" onClick={() => toggleExpansion(!isExpanded)} >
+        <span></span>
+      </button>
+      <nav className="navigation">
+        <AniLink cover
+          to="/tech"
+          direction="right"
+          duration={1}
+          bg="#ff414e"
+          activeClassName="active"
+          >Tech stack</AniLink>
+        <AniLink cover
+          to="/projects"
+          direction="right"
+          duration={1}
+          bg="#ff414e"
+          activeClassName="active"
+          >Projects</AniLink>
+        <AniLink cover
+          to="/contact"
+          direction="right"
+          duration={1}
+          bg="#ff414e"
+          activeClassName="active"
+          >Get in touch</AniLink>
+      </nav>
+    </div>
+  )
+}
+export default Navigation
