f72ccdf999f8b2cf163476f1b8f5bec5cc0947f3
Simon Lawrenson <simon@optimising.com.au>
2020-02-05 09:30:32 +1100

    Remove inline page container module

--- src/components/pageContainer.js
+++ src/components/pageContainer.js
@@ -1,8 +1,7 @@
 import React from "react"
-import containerStyles from "../styles/pageContainer.module.css"
 
 export default ({ children }) => (
-  <main className={containerStyles.pageContainer}>
+  <main className="page-container">
     {children}
   </main>
 )
