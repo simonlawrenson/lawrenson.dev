f9ce62f684b81a65f2430eb6e70ee95b32954eb2
Simon Lawrenson <simon@optimising.com.au>
2020-02-05 11:34:07 +1100

    Add social footer links

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,16 @@
 import React from "react"
-  
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import '../utils/fontawesome'
+
 export default() =>
   <footer>
     <div className="container container-footer">
       <div className="col">
         <p>Email: <a href="mailto:hello@lawrenson.dev">hello@lawrenson.dev</a> </p>
+      </div>
+      <div className="col col-social">
+        <p><a href="https://www.linkedin.com/in/simon-lawrenson-ba40b537" target="_blank" rel="noopener noreferrer" title="LinkedIn"><FontAwesomeIcon icon={['fab', 'linkedin-in']} / ></a> </p>
+        <p><a href="https://bitbucket.org/simonlawrenson" target="_blank" rel="noopener noreferrer" title="Bitbucket"><FontAwesomeIcon icon={['fab', 'bitbucket']} / ></a> </p>
       </div>
     </div>
   </footer>
--- src/utils/fontawesome.js
+++ src/utils/fontawesome.js
@@ -3,7 +3,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 // import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 // import { faBars } from '@fortawesome/pro-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import { faLink, faLongArrowAltRight } from '@fortawesome/pro-regular-svg-icons';
+import { faLink, faLongArrowAltRight, faHeart } from '@fortawesome/pro-regular-svg-icons';
 
 /* @fortawesome/fontawesome-pro */
-library.add(fab, faLink, faLongArrowAltRight)
+library.add(fab, faLink, faLongArrowAltRight, faHeart)
